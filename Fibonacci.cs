e802a9b15158c991f1d5f3c166078c86800b0afb
Hélio Nunes <helioaugustofn@protonmail.com>
2022-02-23 21:45:24 -0400

    Adicionando mais resoluções de exercícios

---
+++ Fibonacci.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algoritmos
+{
+    internal class Fibonacci
+    {
+        static void Main(string[] args)
+        {
+            int anterior = 0;
+            int atual = 1;
+            int proximo;
+            int i = 0;
+            Console.WriteLine("Insira até qual o termo deseja calcular: ");
+            int n = int.Parse(Console.ReadLine());
+
+            while (i < n)
+            {
+                proximo = anterior + atual;
+                Console.WriteLine(proximo);
+                anterior = atual;
+                atual = proximo;
+                i++;
+            }
+
+        }
+    }
+}
+  
---
+++ TabuleiroXadrez.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algoritmos
+{
+    internal class TabuleiroXadrez
+    {
+        static void Main(string[] args)
+        {
+            int s;
+            int a = 1;
+            for (int i = 1; i <= 64; i++)
+            {
+                Console.WriteLine(a);
+                s = 2 * (int)Math.Pow(a, 2);
+                a += s;
+            }
+            //Console.WriteLine($"O Somatório dos grãos é = {s}");
+        }
+    }
+}
